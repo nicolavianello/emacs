44946211f1054e5cde40cd08a2c4ce634e7adf66
Nicola Vianello <nicola.vianello@gmail.com>
2013-06-12 13:34:49 +0200

    changed conf/install-sy.el

--- conf/install-sy.el
+++ conf/install-sy.el
@@ -1,21 +1,19 @@
 ;/------ Conditionally check if emacs is 24 or 23. If 23
 ; it is needed the package.el packaged available at http://bit.ly/pkg-el23
-(if (>= emacs-major-version 24)
-    (progn
-      (require 'package)
-      (setq package-archives '(
-					;    ("ELPA" . "http://tromey.com/elpa/")
-					;    ("gnu" . "http://elpa.gnu.org/packages/")
-			       ("marmalade" . "http://marmalade-repo.org/packages/")
-			       ("org" . "http://orgmode.org/elpa/")
-			       ("melpa" . "http://melpa.milkbox.net/packages/")))
-      (package-initialize)
-      )
-  (require 'package)
-  ;; Any add to list for package-archives (to add marmalade or melpa) goes here
-  (add-to-list 'package-archives 
-	       '("marmalade" . "http://marmalade-repo.org/packages/")
-	       '("org" . "http://orgmode.org/elpa/")
-	       '("melpa" . "http://melpa.milkbox.net/packages/"))
-  (package-initialize)  ;; Do something else for Emacs 23 or less
-)
+(when (>= emacs-major-version 23)
+
+  (when (<= emacs-major-version 24)
+    (require 'package)
+    )
+
+  (setq package-archives '(
+                           ("marmalade" . "http://marmalade-repo.org/packages/")
+			   ("org" . "http://orgmode.org/elpa/")
+			   ("melpa" . "http://melpa.milkbox.net/packages/")
+                           ))
+
+  (package-initialize)
+  )
+
+
+
